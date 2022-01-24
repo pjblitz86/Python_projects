099deac604b15ad2350623bc5df8dcf16882e928
pjblitz86 <paulius.juknis@gmail.com>
2022-01-24 13:49:32 +0200

    lower/upper case impro

--- simplest_translator/app.py
+++ simplest_translator/app.py
@@ -1,8 +1,11 @@
 def translate(phrase):
     translation = ""
     for letter in phrase:
-        if letter in "AEIOUaeiou":
-            translation += "g"
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation += "G"
+            else:
+                translation += "g"
         else:
             translation += letter
     return translation
